fac487c2a7154c6b6a5e34acebd634cf25081b68
Francisco Hernandez <frhernandez@csumb.edu>
2018-10-02 17:57:33 -0700

    Initial Commit

---
+++ ParseChat/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ParseChat
+//
+//  Created by Francisco Hernanedez on 10/2/18.
+//  Copyright © 2018 Francisco Hernanedz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
